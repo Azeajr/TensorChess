0b63cec4a7d41d097a201f136fb53d9928368966
Antonio Zea Jr <zeajantonio@gmail.com>
2023-03-23 00:28:07 -0400

    feat(chess_moves): :sparkles: make Validtor into abstract class

--- tensor_chess/app/chess_moves.py
+++ tensor_chess/app/chess_moves.py
@@ -1,41 +1,63 @@
 from __future__ import annotations
-from dataclasses import dataclass, field
-from typing import Callable, TYPE_CHECKING
+from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
 
 
 if TYPE_CHECKING:
     from chess_board import Square
+    from chess_set import Piece
 
 
-@dataclass
-class Validators:
+def get_validator(name: str) -> Validator:
+    """Returns a validator for a chess piece."""
+    validators = {
+        "pawn": PawnValidator,
+        "rook": RookValidator,
+        "knight": KnightValidator,
+        "bishop": BishopValidator,
+        "queen": QueenValidator,
+        "king": KingValidator,
+    }
+    return validators[name]
+
+
+class Validator(ABC):
     """Represents a chess piece validator."""
 
-    grid: list[list[Square]]
-    validator: dict[str, Callable[[Square, Square], bool]] = field(default_factory=dict)
+    @abstractmethod
+    def validate(self, start: Square, end: Square) -> bool:
+        """Validates a move."""
 
-    def _pawn_validator(self, start: Square, end: Square, direction: int) -> bool:
+
+class PawnValidator(Validator):
+    """Represents a pawn validator."""
+
+    def __init__(self, grid: list[list[Square]]):
+        """Initializes the pawn validator."""
+        self.grid = grid
+
+    def validate(self, start: Square, end: Square) -> bool:
         """Validates a pawn move."""
 
         def __single_move() -> bool:
             """Validates a single move."""
             return (
                 start.cord.x == end.cord.x
-                and start.cord.y + 1 * direction == end.cord.y
+                and start.cord.y + 1 * start.piece.direction == end.cord.y
                 and not end.piece
             )
 
         def __double_move() -> bool:
             """Validates a double move."""
-            if direction == 1:
+            if start.piece.direction == 1:
                 if start.cord.y != 1:
                     return False
-            elif direction == -1:
+            elif start.piece.direction == -1:
                 if start.cord.y != 6:
                     return False
             return (
                 start.cord.x == end.cord.x
-                and start.cord.y + 2 * direction == end.cord.y
+                and start.cord.y + 2 * start.piece.direction == end.cord.y
                 and not end.piece
             )
 
@@ -43,59 +65,55 @@ class Validators:
             """Validates a capture move."""
             return (
                 abs(start.cord.x - end.cord.x) == 1
-                and start.cord.y + 1 * direction == end.cord.y
+                and start.cord.y + 1 * start.piece.direction == end.cord.y
                 and end.piece
             )
-        
+
         def __en_passant() -> bool:
             """Validates an en passant move."""
-            if direction == 1:
+            if start.piece.direction == 1:
                 if start.cord.y != 4:
                     return False
-            elif direction == -1:
+            elif start.piece.direction == -1:
                 if start.cord.y != 3:
                     return False
             return (
                 abs(start.cord.x - end.cord.x) == 1
-                and start.cord.y + 1 * direction == end.cord.y
+                and start.cord.y + 1 * start.piece.direction == end.cord.y
                 and not end.piece
                 and self.grid[end.cord.x][start.cord.y].piece
             )
 
         return __single_move() or __double_move() or __capture() or __en_passant()
 
-    def _rook_validator(self, start: Square, end: Square, _) -> bool:
-        """Validates a rook move."""
-        if start.cord.x == end.cord.x:
-            for y in range(
-                min(start.cord.y, end.cord.y) + 1, max(start.cord.y, end.cord.y)
-            ):
-                if self.grid[start.cord.x][y].piece:
-                    return False
-            return True
-        elif start.cord.y == end.cord.y:
-            for x in range(
-                min(start.cord.x, end.cord.x) + 1, max(start.cord.x, end.cord.x)
-            ):
-                if self.grid[x][start.cord.y].piece:
-                    return False
-            return True
 
-    def _knight_validator(self, start: Square, end: Square, _) -> bool:
+class KnightValidator(Validator):
+    """Represents a knight validator."""
+
+    def validate(self, start: Square, end: Square) -> bool:
         """Validates a knight move."""
         return (
-            abs(start.cord.x - end.cord.x) == 2
-            and abs(start.cord.y - end.cord.y) == 1
-            or abs(start.cord.x - end.cord.x) == 1
+            abs(start.cord.x - end.cord.x) == 1
             and abs(start.cord.y - end.cord.y) == 2
+            or abs(start.cord.x - end.cord.x) == 2
+            and abs(start.cord.y - end.cord.y) == 1
         )
 
-    def _bishop_validator(self, start: Square, end: Square, _) -> bool:
+
+class BishopValidator(Validator):
+    """Represents a bishop validator."""
+
+    def __init__(self, grid: list[list[Square]]):
+        """Initializes the bishop validator."""
+        self.grid = grid
+
+    def validate(self, start: Square, end: Square) -> bool:
         """Validates a bishop move."""
+
         def slope(start: Square, end: Square) -> float:
             """Returns the slope of the line between two squares."""
             return (end.cord.y - start.cord.y) / (end.cord.x - start.cord.x)
-        
+
         if abs(start.cord.x - end.cord.x) == abs(start.cord.y - end.cord.y):
             if slope(start, end) == 1.0:
                 for x, y in zip(
@@ -115,7 +133,9 @@ class Validators:
                         min(start.cord.x, end.cord.x) + 1, max(start.cord.x, end.cord.x)
                     ),
                     range(
-                        max(start.cord.y, end.cord.y) - 1, min(start.cord.y, end.cord.y), -1
+                        max(start.cord.y, end.cord.y) - 1,
+                        min(start.cord.y, end.cord.y),
+                        -1,
                     ),
                 ):
                     if self.grid[x][y].piece:
@@ -123,24 +143,55 @@ class Validators:
                 return True
             return False
 
-    def _queen_validator(self, start: Square, end: Square, _) -> bool:
+
+class RookValidator(Validator):
+    """Represents a rook validator."""
+
+    def __init__(self, grid: list[list[Square]]):
+        """Initializes the rook validator."""
+        self.grid = grid
+
+    def validate(self, start: Square, end: Square) -> bool:
+        """Validates a rook move."""
+        if start.cord.x == end.cord.x:
+            for y in range(
+                min(start.cord.y, end.cord.y) + 1, max(start.cord.y, end.cord.y)
+            ):
+                if self.grid[start.cord.x][y].piece:
+                    return False
+            return True
+        elif start.cord.y == end.cord.y:
+            for x in range(
+                min(start.cord.x, end.cord.x) + 1, max(start.cord.x, end.cord.x)
+            ):
+                if self.grid[x][start.cord.y].piece:
+                    return False
+            return True
+
+
+class QueenValidator(Validator):
+    """Represents a queen validator."""
+
+    def __init__(self, grid: list[list[Square]]):
+        """Initializes the queen validator."""
+        self.grid = grid
+        self.bishop = BishopValidator(grid)
+        self.rook = RookValidator(grid)
+
+    def validate(self, start: Square, end: Square) -> bool:
         """Validates a queen move."""
-        return self._rook_validator(start, end, None) or self._bishop_validator(
-            start, end, None
-        )
+        return self.bishop.validate(start, end) or self.rook.validate(start, end)
 
-    def _king_validator(self, start: Square, end: Square, _) -> bool:
+
+class KingValidator(Validator):
+    """Represents a king validator."""
+
+    def __init__(self, grid: list[list[Square]]):
+        """Initializes the king validator."""
+        self.grid = grid
+
+    def validate(self, start: Square, end: Square) -> bool:
         """Validates a king move."""
-        return (
-            abs(start.cord.x - end.cord.x) <= 1 and abs(start.cord.y - end.cord.y) <= 1
-        )
-
-    def __post_init__(self):
-        """Loads the validator function."""
-        self.validator["pawn"] = self._pawn_validator
-        self.validator["rook"] = self._rook_validator
-        self.validator["knight"] = self._knight_validator
-        self.validator["bishop"] = self._bishop_validator
-        self.validator["queen"] = self._queen_validator
-        self.validator["king"] = self._king_validator
-        
+        if abs(start.cord.x - end.cord.x) <= 1 and abs(start.cord.y - end.cord.y) <= 1:
+            return True
+        return False
